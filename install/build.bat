ae4ae86a29692030499f410b7346a12b7a3ce863
Rostyslav Lyulinetskyy <rostik@dicehub.com>
2017-12-15 13:05:41 +0100

    Fixed python_occ app deployment

--- install/build.bat
+++ install/build.bat
@@ -7,5 +7,5 @@ cd %~dp0
 http://backup1.foxcloud.net/mcubcsro/public/dice/wheels/numpy_stl-2.3.2-cp36-cp36m-win_amd64.whl ^
 http://backup1.foxcloud.net/mcubcsro/public/dice/wheels/pythonocc_core-0.18.0-cp36-cp36m-win_amd64.whl ^
 http://backup1.foxcloud.net/mcubcsro/public/dice/wheels/vtk-8.0.0-cp36-cp36m-win_amd64.whl ^
-https://github.com/dicehub/dice_tools/archive/17.10.0.zip ^
-https://github.com/dicehub/dice_vtk/archive/17.10.0.zip
+https://github.com/dicehub/dice_tools/archive/17.12.0.zip ^
+https://github.com/dicehub/dice_vtk/archive/17.12.0.zip
